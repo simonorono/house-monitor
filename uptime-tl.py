0ee594cc76e62a5041389bf5b2e66cec026dd1d8
Simón Oroño <simonorono@protonmail.com>
2024-01-02 14:06:24 -0400

    Implement function to form the duration message

--- uptime-tl.py
+++ uptime-tl.py
@@ -8,5 +8,25 @@ def get_uptime() -> int:
         raise "Couldn't read uptime from /proc/uptime"
 
 
+def get_duration_message(seconds: int) -> str:
+    parts = [
+        [seconds // 60 // 60 // 24, "day"],
+        [seconds // 60 // 60 % 24, "hour"],
+        [seconds // 60 % 60, "minute"],
+        [seconds % 60, "second"]
+    ]
+
+    parts = [x for x in parts if x[0] > 0]
+
+    if len(parts) == 0:
+        return "1 second"
+
+    for part in parts:
+        if part[0] != 1:
+            part[1] = f"{part[1]}s"
+
+    return ' '.join([f'{p[0]} {p[1]}' for p in parts])
+
+
 if __name__ == '__main__':
-    print(get_uptime())
+    print(get_duration_message(get_uptime()))
