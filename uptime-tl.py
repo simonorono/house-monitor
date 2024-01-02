bcb28a57a493a6fcba1afe939e876d1ff3bf3b91
Simón Oroño <simonorono@protonmail.com>
2024-01-02 14:02:44 -0400

    Implement reading of system's uptime

--- uptime-tl.py
+++ uptime-tl.py
@@ -1,2 +1,12 @@
+def get_uptime() -> int:
+    with open('/proc/uptime', 'r') as file:
+        content = file.read()
+
+    if content is not None:
+        return int(float(content.split(" ")[0]))
+    else:
+        raise "Couldn't read uptime from /proc/uptime"
+
+
 if __name__ == '__main__':
-    print("Hello, world!")
+    print(get_uptime())
