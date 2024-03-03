a087fb5fff132018a4c97bde1bd44f370669af26
Simón Oroño <simonorono@protonmail.com>
2024-03-03 17:07:07 -0400

    Start re-writing in Rust

---
+++ src/main.rs
@@ -0,0 +1,71 @@
+use std::collections::HashMap;
+use std::path::Path;
+use std::{env, fs};
+
+const ERROR_CONFIG_FILE_UNREADABLE: &str = "couldn't read config file";
+const ERROR_HOME_VARIABLE_NOT_SET: &str = "couldn't find home env variable";
+const ERROR_UNABLE_TO_READ_FILE: &str = "couldn't read uptime file";
+const ERROR_UPTIME_NOT_FLOAT: &str = "uptime value is not decimal";
+
+fn read_config() -> HashMap<String, String> {
+    let home = env::var("HOME").expect(ERROR_HOME_VARIABLE_NOT_SET);
+    let file = Path::new(&home).join(".config").join("house-monitor.conf");
+    let config_file = fs::read_to_string(file).expect(ERROR_CONFIG_FILE_UNREADABLE);
+
+    let mut config = HashMap::new();
+
+    config_file.lines().for_each(|line| {
+        let parts = line
+            .split("=")
+            .map(|s| s.to_string())
+            .collect::<Vec<String>>();
+
+        config.insert(format!("{}", parts[0]), format!("{}", parts[1]));
+    });
+
+    config
+}
+
+fn get_uptime() -> u64 {
+    let uptime_file_content = fs::read_to_string("/proc/uptime").expect(ERROR_UNABLE_TO_READ_FILE);
+
+    uptime_file_content
+        .split_whitespace()
+        .collect::<Vec<&str>>()[0]
+        .parse::<f64>()
+        .expect(ERROR_UPTIME_NOT_FLOAT) as u64
+}
+
+fn get_duration_message(seconds: u64) -> String {
+    [
+        (seconds / 60 / 60 / 24, "day"),
+        (seconds / 60 / 60 % 24, "hour"),
+        (seconds / 60 % 60, "minute"),
+        (seconds % 60, "second"),
+    ]
+    .iter()
+    .filter(|(x, _)| *x > 0)
+    .map(|(x, y)| {
+        let unit = if (*x) <= 1 {
+            (*y).to_string()
+        } else {
+            format!("{}s", y)
+        };
+
+        (*x, unit)
+    })
+    .map(|(x, y)| format!("{} {}", x, y))
+    .collect::<Vec<String>>()
+    .join(" ")
+}
+
+fn send_message(message: String) {
+    let config = read_config();
+
+    println!("{:?}", config);
+    println!("{}", message);
+}
+
+fn main() {
+    send_message(get_duration_message(get_uptime()))
+}
